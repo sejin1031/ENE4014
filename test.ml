04c1fb5c171a68f51d7b478d46469adb58a0fab8
sejin.eom <sejin1031@utplus.co.kr>
2021-04-17 20:53:26 +0900

    test for sigma

--- test.ml
+++ test.ml
@@ -1,11 +1,26 @@
-let slice s pos len =
-      if (String.length s) <= 1 then ""
-      else (String.sub s pos len)
+type exp = X | INT of int
+| REAL of float
+| ADD of exp * exp
+| SUB of exp * exp 
+| MUL of exp * exp
+| DIV of exp * exp
+| SIGMA of exp * exp * exp
+| INTEGRAL of exp * exp * exp
 
-let rec dist a b =
-    if (a = "") then (String.length b)
-    else if (b = "") then (String.length a)
-    else if (String.get a 0) = (String.get b 0) then dist (slice a 1 ((String.length a) - 1)) (slice b 1 ((String.length b) - 1))
-    else (1 + min (min (dist (slice a 1 ((String.length a) - 1)) (slice b 1 ((String.length b) - 1)))
-                (dist a (slice b 1 ((String.length b) - 1))) ) 
-                (dist (slice a 1 ((String.length a) - 1)) b));
+let rec calculate exp = 
+  match exp with
+  | X -> exp
+  | INT x -> x
+  | REAL x -> int_of_float x
+  | ADD (e1, e2) -> (calculate e1) + (calculate e2)
+  | SUB (e1, e2) -> (calculate e1) - (calculate e2)
+  | MUL (e1, e2) -> (calculate e1) - (calculate e2)
+  | DIV (e1, e2) -> (calculate e1) - (calculate e2)
+  | SIGMA (e1, e2, e3) -> calculate (sigma e1 e2 e3 (INT 0))
+and calsigma c ex=
+	match ex with
+	X -> calculate c
+	| _ -> calculate ex
+and sigma c e ex acc =
+	if calculate(c) = calculate(e) then acc
+	else sigma ADD(c, INT 1) e ex (ADD (acc, (calsigma c ex)))
