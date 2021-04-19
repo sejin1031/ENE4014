66f8d8c2ed01188a58686bc6b346282113708ff4
sejin1031 <sejin1031@utplus.co.kr>
2021-04-19 19:36:23 +0900

    .

--- hw2.ml
+++ hw2.ml
@@ -7,18 +7,41 @@ type exp = X | INT of int
 | SIGMA of exp * exp * exp
 | INTEGRAL of exp * exp * exp
 
-(* let rec calculate exp = 
+let calculate exp = 
+  let cur_val = ref 1.1 in
+  let rec cal exp =
   match exp with
-  | X 
-  | INT x -> Int x
-  | REAL x -> Float x
-  | ADD (e1, e2) -> (calculate e1) + (calculate e2)
-  | SUB (e1, e2) -> (calculate e1) - (calculate e2) *)
+  | X -> !cur_val
+  | INT x -> float_of_int x
+  | REAL x -> x
+  | ADD (e1, e2) -> cal e1 +. cal e2
+  | SUB (e1, e2) -> cal e1 -. cal e2
+  | MUL (e1, e2) -> cal e1 *. cal e2
+  | DIV (e1, e2) -> cal e1 /. cal e2
+  | SIGMA (s ,e, ex) -> (sigma s e ex 0.0)
+  | INTEGRAL (a,b,c) -> 0.0
+  and sigma current last exp acc=
+    if (cal current) > (cal last) then (acc)
+    else (
+      cur_val := cal(current);
+      sigma (ADD(current, INT 1)) last exp (acc +. (cal exp))
+    )
+    in cal exp;
 
-type bigint = BigInt of string
-type op = ADD | MUL
+(* type ae = CONST of int
+  | VAR of string
+  | POWER of string * int
+  | TIMES of ae list
+  | SUM of ae list *)
+(* let diff d =  *)
 
-(* let compute_bigint op b1 b2 = *)
+(* type bigint = BigInt of string
+type op = ADD | MUL *)
+
+(* let compute_bigint op b1 b2 = 
+  let rec plus str1 str2 result
+  match op with
+  | ADD ->  *)
 
 
 
@@ -29,11 +52,11 @@ let count_string s x =
       else if String.equal (String.sub s 0 len) x
       then str (String.sub s 1 ((String.length s) - 1)) x (count + 1)
       else str (String.sub s 1 ((String.length s) - 1)) x count
-    in str s x 0
+    in str s x 0;
 
 let rec pascal (a,b) =
   if a = b  || b = 0 then 1
-  else pascal(a-1,b) + pascal(a-1,b-1)
+  else pascal(a-1,b) + pascal(a-1,b-1);
 
 
 let closest str strlist =
@@ -54,20 +77,20 @@ let closest str strlist =
     | [] -> best_content
     | hd::tl -> if((dist s hd) < best) then clo str tl (dist s hd) hd
                 else clo str tl best best_content in
-      clo str strlist Int.max_int ""
+      clo str strlist Int.max_int "";
 
 
 
 let equals v1 v2 = 
-    v1 = v2
+    v1 = v2;
 
 let test t1 t2 answer =
   let v = closest t1 t2 in
-  (equals v answer)
+  (equals v answer);
 
 
 
-type relationships = (string * string) list
+type relationships = (string * string) list;
 
 
 
--- test.ml
+++ test.ml
@@ -1,11 +1,29 @@
-let slice s pos len =
-      if (String.length s) <= 1 then ""
-      else (String.sub s pos len)
+type exp = X | INT of int
+| REAL of float
+| ADD of exp * exp
+| SUB of exp * exp
+| MUL of exp * exp
+| DIV of exp * exp
+| SIGMA of exp * exp * exp
+| INTEGRAL of exp * exp * exp
 
-let rec dist a b =
-    if (a = "") then (String.length b)
-    else if (b = "") then (String.length a)
-    else if (String.get a 0) = (String.get b 0) then dist (slice a 1 ((String.length a) - 1)) (slice b 1 ((String.length b) - 1))
-    else (1 + min (min (dist (slice a 1 ((String.length a) - 1)) (slice b 1 ((String.length b) - 1)))
-                (dist a (slice b 1 ((String.length b) - 1))) ) 
-                (dist (slice a 1 ((String.length a) - 1)) b));
+let calculate exp = 
+  let cur_val = ref 1.1 in
+  let rec cal exp =
+  match exp with
+  | X -> !cur_val
+  | INT x -> float_of_int x
+  | REAL x -> x
+  | ADD (e1, e2) -> cal e1 +. cal e2
+  | SUB (e1, e2) -> cal e1 -. cal e2
+  | MUL (e1, e2) -> cal e1 *. cal e2
+  | DIV (e1, e2) -> cal e1 /. cal e2
+  | SIGMA (s ,e, ex) -> (sigma s e ex 0.0)
+  | INTEGRAL (a,b,c) -> 0.0
+  and sigma current last exp acc=
+    if (cal current) > (cal last) then (acc)
+    else (
+      cur_val := cal(current);
+      sigma (ADD(current, INT 1)) last exp (acc +. (cal exp))
+    )
+    in cal exp
